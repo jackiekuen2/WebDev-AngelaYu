8c1809e0c0a5592ba065c7cfd7ae30d5c8e0a591
Jackie Lau <jackiekuen2@gmail.com>
2021-10-22 09:37:22 +0800

    NodeJS Weather Web App using API

---
+++ Nodejs-WeatherProject/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Root Route
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    console.log(req.body.cityName);
+
+    const query = req.body.cityName // Name of the input
+    const apiKey = "YOUR-API-KEY"
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + apiKey
+
+    https.get(url, function(response) {
+    console.log(response.statusCode);
+
+        // Access the data from response
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data); //Parsing weather data
+            const temp = weatherData.main.temp;
+            const desc = weatherData.weather[0].description;
+            const iconCode = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + iconCode +"@2x.png"
+            
+            res.write("<p>The weather is currently " + desc + "</p>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degree Celcius.</h1>");
+            res.write('<img src="' + imageURL + '">');
+            res.send();
+        })
+    });
+});
+
+app.listen(port, function() {
+    console.log("Server is running on port " + port);
+});
